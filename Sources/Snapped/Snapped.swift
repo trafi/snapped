716ce3bd130f35e5d6aa3d6ab9cd1770312ff066
Eimantas Vaičiūnas <eimantas@stork.software>
2020-11-30 17:20:25 +0200

    Make two targets, remove tests

--- Sources/Snapped/Snapped.swift
+++ Sources/Snapped/Snapped.swift
@@ -1,3 +1,25 @@
-struct Snapped {
-    var text = "Hello, World!"
+import SwiftUI
+
+public protocol Snapped {
+    associatedtype SnapView: View
+    static var snapped: [String: SnapView] { get }
+    static var layout: PreviewLayout { get }
+    static var paddingEdges: Edge.Set { get }
+}
+
+public extension Snapped {
+
+    static var layout: PreviewLayout { .sizeThatFits }
+    static var paddingEdges: Edge.Set { .all }
+}
+
+public extension Snapped where Self: PreviewProvider {
+
+    static var previews: some View {
+        ForEach(snapped.keys.sorted(), id: \.self) {
+            snapped[$0]?.previewDisplayName($0.capitalized)
+        }
+        .padding(paddingEdges)
+        .previewLayout(layout)
+    }
 }
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import SnappedTests
-
-var tests = [XCTestCaseEntry]()
-tests += SnappedTests.allTests()
-XCTMain(tests)
